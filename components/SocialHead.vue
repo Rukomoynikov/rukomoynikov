3adb8f412d9ac58c2954d9e683c362d08bc3c74d
Maksim R <maksimr@MacBook-Pro-Maksim.local>
2021-10-04 14:20:17 +0300

    Twitter card

--- components/SocialHead.vue
+++ components/SocialHead.vue
@@ -27,7 +27,7 @@ export default {
         {
           hid: 'twitter:card',
           name: 'twitter:card',
-          content: 'summary_large_image'
+          content: 'summary'
         },
         {
           hid: 'twitter:title',
