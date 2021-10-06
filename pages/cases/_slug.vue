66d7e81b704824aa9dc287d9331549ce89bdf4a9
Maksim R <maksimr@MacBook-Pro-Maksim.local>
2021-10-06 12:30:43 +0300

    Cases

---
+++ pages/cases/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ this.slug }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ params }) {
+    const slug = params.slug // When calling /abc the slug will be "abc"
+    return { slug }
+  }
+})
+</script>
