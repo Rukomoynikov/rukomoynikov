f75e013ea3d0a9a6c7ee5425263c2c5de82221b6
Maksim R <maksimr@MacBook-Pro-Maksim.local>
2021-10-06 12:44:51 +0300

    English

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  generate: {
+    routes: ['/cases/alfa']
+  },
+
   router: {
     base: '/rukomoynikov/'
   },
