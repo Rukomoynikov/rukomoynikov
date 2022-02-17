23da75782d707a8be509c0b86314bf5814555705
Maksim Rukomoynikov <maksim@rukomoynikov.ru>
2022-02-17 22:10:06 +0300

    Few more cases

---
+++ components/case/Case.stories.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'Posts/Case'
+}
+
+export const Case = () => `
+  <Case
+    :tags="['Design']"
+    :description="$t('Site and boilerplate for creatting Vue applciations. Design of the site based on technical specifications familiar for every javascript developer.')"
+    :href-title="'Site'"
+    :title="$t('Boilerplate for Vue apps')"
+    :image-src="'cases/vue-boilerpate/vue-boilerpate'"
+    :case-href="'https://rukomoynikov.github.io/vue-webpack-boilerplate/'"
+  />
+`
