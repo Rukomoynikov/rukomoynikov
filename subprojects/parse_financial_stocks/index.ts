0d93219ed8e314ed1924b573b91cab3b7587b6e1
Maksim Rukomoynikov <maksim@rukomoynikov.ru>
2022-02-28 21:02:39 +0300

    Parse markets

--- subprojects/parse_financial_stocks/index.ts
+++ subprojects/parse_financial_stocks/index.ts
@@ -11,18 +11,30 @@ interface MarketSymbol {
   volume: number;
 }
 
-class Parser {
-  constructor (private api_key: string) {
+class MarketParser {
+  base_url = "https://eodhistoricaldata.com/api"
 
+  constructor (private api_key: string) {}
+
+  async historical_values (symbol: string): Promise<MarketSymbol[]> {
+    const url = `${this.base_url}/eod/${symbol}`;
+    const request_params = {
+      api_token: this.api_key,
+      period: "d",
+      fmt: "json"
+    }
+
+    const response = await axios.get(url, {params: request_params});
+
+    return response.data
   }
+}
 
-  async historical_values (symbol: string) {
-    const url = `https://eodhistoricaldata.com/api/eod/${symbol}?api_token=${this.api_key}&period=d&fmt=json`;
-    const response = await axios.get(url);
+class FilesHandler {
+  static write (file_name: string, data: string | { [key: string]: any }) {
+    const stringifiedData = JSON.stringify(data);
 
-    const stringifiedData = JSON.stringify(response.data);
-
-    fs.writeFile(`./subprojects/parse_financial_stocks/${symbol}.json`, stringifiedData, (err: any) => {
+    fs.writeFile(file_name, stringifiedData, (err: any) => {
       if (err) {
         console.log(err)
       }
@@ -33,9 +45,13 @@ class Parser {
 }
 
 async function main () {
-  const parser = new Parser("621c7ba1d67437.83853199")
-  parser.historical_values("AAPL.US")
-}
+  const parser = new MarketParser("621c7ba1d67437.83853199")
+  const symbols = ["AAPL.US", "TSLA.US"]
 
+  for await (const symbol of symbols) {
+    const data = await parser.historical_values(symbol)
+    FilesHandler.write(`./subprojects/parse_financial_stocks/${symbol}.json`, data)
+  }
+}
 
 main()
