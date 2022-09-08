8a73885b8f8370b2c2df04f4204361b864d01a59
Max Rukomoynikov <max@apply4.com>
2022-09-08 17:43:42 +0400

    Site config option

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [image(), svelte()],
+  site: "https://rukomoynikov.ru"
 });
+
+
+// import.meta.env.BASE_URL
