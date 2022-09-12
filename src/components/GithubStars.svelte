8377fc5799578ff56c624c524c713dac99289878
Max Rukomoynikov <max@apply4.com>
2022-09-12 18:45:26 +0400

    Github block

---
+++ src/components/GithubStars.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let repo: string;
+
+  let repo_info: string;
+  let data: string;
+
+  async function main() {
+    data = await fetch(`https://api.github.com/repos/${repo}`).then(response =>response.json());
+  }
+
+  main()
+</script>
+
+{JSON.stringify(data)}
