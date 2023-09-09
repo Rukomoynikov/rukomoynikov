2f11d9f9fe94674ba6ad34eed93eb424063523ef
Maksim Rukomoynikov <max@apply4.com>
2023-09-09 17:26:03 +0100

    Main page

---
+++ src/components/ScrollObserver.svelte
@@ -0,0 +1,13 @@
+<script>
+  let y;
+
+  function scrollObserver(event) {
+    console.log(event)
+  }
+</script>
+
+<main>
+  {y}
+</main>
+
+<svelte:window bind:scrollY={y} on:scroll="{scrollObserver}"/>
