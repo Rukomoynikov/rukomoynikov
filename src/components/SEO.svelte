0ae364fd239d425a3f45593b9f7a3fbba3bf92b9
Max Rukomoynikov <max@apply4.com>
2024-01-01 16:13:05 +0000

    Update SEO component

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -2,13 +2,13 @@
 	import SvelteSeo from 'svelte-seo';
 	import DefaultSocialIcon from '/src/assets/images/facebook_share/facebook_cover_1200x630.jpg?h=630&w=1200&url';
 
-	export let title: string | undefined;
-	export let description =
-		"Hey there. It's a page of a regular developer. Mostly I write about ruby or typescript.";
+	export let title: string;
+	export let description: string;
 
 	export let image = DefaultSocialIcon;
 
 	$: metaTitle = title ? `${title} | Max Rukomoynikov` : 'Max Rukomoynikov';
+  $: metaDescription = description || 'Max Rukomoynikov. Software Engineer. Ruby, Rails, Typescript, React, Elixir, Rust';
 	$: socialImage = `https://rukomoynikov.ru${image}`;
 
 	import { page } from '$app/stores';
@@ -16,28 +16,28 @@
 
 <SvelteSeo
 	title={metaTitle}
-	{description}
-	canonical="https://rukomoynikov.ru/"
+	description={metaDescription}
+	canonical={$page.url.pathname}
 	keywords="rails, ruby, typescript, react, elixir, rust"
 	openGraph={{
 		title: metaTitle,
-		description,
+		description: metaDescription,
 		images: [
 			{
 				url: socialImage,
 				width: 1200,
 				height: 630,
-				alt: "Max Rukomoynikov's site"
+				alt: metaTitle
 			}
 		],
 		url: $page.url.pathname,
 		type: 'website',
-		site_name: "Max Rukomoynikov's site"
+		site_name: metaTitle
 	}}
 	twitter={{
 		card: 'summary_large_image',
 		site: '@MRukomoynikov',
-		description,
+		description: metaDescription,
 		image: DefaultSocialIcon
 	}}
 />
