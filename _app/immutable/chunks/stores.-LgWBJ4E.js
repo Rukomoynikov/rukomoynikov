03d79af172f2ab590d1bf3177e70a3e4e6b47a35
Rukomoynikov <cat@mos-it.com>
2024-01-01 14:27:58 +0000

    Deploying to gh-pages from @ Rukomoynikov/rukomoynikov@aea74a474440252d219c4fe9a1ecb89965e6da8f 🚀

---
+++ _app/immutable/chunks/stores.-LgWBJ4E.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.8w5h5bU2.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
