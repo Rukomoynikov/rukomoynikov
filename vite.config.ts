bd81b53db2a16ca689c3e39ac5373f18962a7150
Max Rukomoynikov <rukomoynikov@gmail.com>
2024-10-24 18:40:27 +0100

    Upgrade to new Svelte version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
+  server: {
+    fs: {
+      allow: [
+        searchForWorkspaceRoot(process.cwd()),
+      ],
+    },
+  },
 	plugins: [sveltekit(), enhancedImages()]
 });
