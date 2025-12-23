51ec3356ad71d471cf42b0c98078ff9b5c701d30
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-12-23 20:18:01 +0000

    Add Tags component for displaying a list of tags

---
+++ src/components/Tags.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let tags: Array<string> = [];
+</script>
+
+<div class="tags">
+  {#each tags as tag}
+    <div class="tags__tag">{tag}</div>
+  {/each}
+</div>
+
+<style>
+  .tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 5px;
+  }
+
+  .tags__tag {
+    display: inline-block;
+    padding: 4px 8px;
+    border-radius: 4px;
+    background: #f1f1f1;
+    font-size: 14px;
+    color: #666;
+  }
+
+  .tags__tag + .tags__tag {
+    margin-left: 2px;
+  }
+</style>
