296a18cee5a6361f0a1a11fafb158749a4084ef9
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-12-23 20:36:38 +0000

    Update GitHub Actions workflow for GitHub Pages deployment

---
+++ artifacter/index.ts
@@ -0,0 +1,6 @@
+// import { createZipUploadStream } from './ziper';
+import { getUploadZipSpecification } from './zip_specifitcation_get';
+
+const searchResult = await findFilesToUpload(inputs.searchPath, inputs.includeHiddenFiles);
+
+// const zipSpecification: UploadZipSpecification[] = getUploadZipSpecification(files, rootDirectory);
